03a29549431a32c718d373457ab4a15ede361e0c
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-02-22 17:11:59 +0700

    create user controllers

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { prisma } from "../config/prisma.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
