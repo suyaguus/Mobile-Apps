063878ef324eadff9dd129c964ee1fa85daddd49
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-02-22 17:13:22 +0700

    fix server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,41 +1,7 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import pkg from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
-
-const { PrismaClient } = pkg;
+import app from "./app.js";
 
 dotenv.config();
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-});
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "API berjalan " });
-});
-
-app.get("/test-db", async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 const PORT = process.env.PORT || 3000;
 
