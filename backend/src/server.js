c221b5d01eacf14e8b18d13f254be3ba2b245cc3
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-02-22 16:53:58 +0700

    add port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,33 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pkg from "@prisma/client";
+
+const { PrismaClient } = pkg;
 
 dotenv.config();
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Finance API Running" });
+  res.json({ message: "API berjalan 🚀" });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.get("/test-db", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
