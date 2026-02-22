bcd1a31d78075906eba1819bed5cc6c868bf658d
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-02-23 04:56:11 +0700

    update user routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getAllUsers } from "../controllers/user.controller.js";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
