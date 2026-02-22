fab30bf2f96ac5b4af8f1be106bee97e128f7e32
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-02-22 17:11:16 +0700

    create config prisma

---
+++ backend/src/config/prisma.js
@@ -0,0 +1,15 @@
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const { PrismaClient } = pkg;
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter,
+});
